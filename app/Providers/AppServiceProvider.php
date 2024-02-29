c6e89b78deb43714d3dd09de2a0d9df3ee3df122
chuongdn02 <chuongnguyendn02@gmail.com>
2024-02-29 22:36:19 +0700

    commit complete deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL; // Add this line to import URL facade
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,11 +19,6 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
-    }
-
-    public function boot()
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
